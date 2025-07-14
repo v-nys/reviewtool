a0b0a6324acea168c31f15bae8a96f9baf2c80d8
Vincent Nys <vincentnys@gmail.com>
2025-07-14 21:25:08 +0200

    Oke, dit is zoals verwacht...

--- markdown_flashcards/markdown_extension_test.py
+++ markdown_flashcards/markdown_extension_test.py
@@ -1,8 +1,18 @@
 from rich.console import Console  # type: ignore
 from rich.markdown import Markdown  # type: ignore
+from markdown_it import MarkdownIt
 
-
-console = Console()
+console = Console(force_terminal=True)
+Markdown.parser = MarkdownIt().enable("strikethrough").enable("table")
 displayed_text = Markdown("""# Test
-Rendert het laatste woord van deze vraag in het {blue|blauw}?""")
+Zie ik het effect van enable (of gebrek daaraan)?
+Tekst met ~~strikethrough~~
+
+| hoofding 1 | hoofding 2 |
+|------------|------------|
+| data 1     | data 2     |
+""")
+# nee, gewoon includen in string zal ook niet gaat
+# Python ziet de escapesequentie, de terminal niet
+# dus zou waarschijnlijk raw characters moeten outputten om dit te laten werken
 console.print(displayed_text)
